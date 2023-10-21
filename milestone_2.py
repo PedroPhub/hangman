702967e416bd964f6a7dc3155bb545b03de33a50
PedroP <pedroncp90@gmail.com>
2023-10-21 21:30:37 +0100

    milestone 2 done

---
+++ milestone_2.py
@@ -0,0 +1,21 @@
+import random
+
+words_list = ['apple',
+              'orange',
+              'banana',
+              'berry',
+              'grape']
+
+def check_len(arg):
+    if len(arg) == 1 & arg.isalpha():
+        print('Good guess!')
+        return 1
+    else:
+        print("Oops! That is not a valid input.")
+        return 0
+
+
+word = random.choice(words_list)
+guess = input('Please enter a letter: ')
+
+check_len(guess)
